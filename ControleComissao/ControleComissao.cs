0fe1ae6f4d24d8df6a0356456ac6c6a92353647b
Elias Romero <102391069+eliasromero72@users.noreply.github.com>
2025-03-25 14:58:32 -0300

    Add files via upload

---
+++ ControleComissao/ControleComissao.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ControleComissao
+{
+    //classe de modelagem do objeto
+    class Vendedor
+    {
+        //atributos
+        private string nome;
+        private double salarioBase;
+
+        //getter's e setter's
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public string getNome()
+        {
+            return nome;
+        }
+        public void setSalarioBase(double salarioBase)
+        {
+            this.salarioBase = salarioBase;
+        }
+        public double getSalarioBase()
+        {
+            return salarioBase;
+        }
+        //método específico da classe
+        public double calcularComissao(double valorVendido)
+        {
+            return valorVendido * 0.03;
+        }
+        //método NÃO específico da classe (exemplo sem retorno)
+        public void calculaComissao2(double valorVendido)
+        {
+            double comissao = valorVendido * 0.03;
+        }
+    }
+}
---
+++ ControleComissao/Principal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControleComissao
+{
+    //classe de manipulação dos objetos
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+            //método construtor
+            Vendedor vendedor1 = new Vendedor();
+
+            //inserindo dados no objeto
+            Console.WriteLine("Digite seu nome: ");
+            vendedor1.setNome(Console.ReadLine());
+
+            Console.WriteLine("Digite seu salário base: ");
+            vendedor1.setSalarioBase(Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("Digite o valor vendido: ");
+            double valorVendido = Convert.ToDouble(Console.ReadLine());
+
+            //exibir os dados
+            Console.WriteLine("Seu nome é: " + vendedor1.getNome());
+            Console.WriteLine("Seu salário base é: " + vendedor1.getSalarioBase());
+
+            //chamando (invocando) o método específico da classe
+            Console.WriteLine("Sua comissão é de: R$ " + vendedor1.calcularComissao(valorVendido));
+
+            //calculando o total
+            Console.WriteLine("Seu salário total é de: R$ " + (vendedor1.getSalarioBase() + vendedor1.calcularComissao(valorVendido)));   
+        }
+    }
+}
